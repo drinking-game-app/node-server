1d18a8481cd10320a61d99bbefb181aa26071539
Eoan O Dea <odeaeoan@gmail.com>
2020-05-27 18:50:26 +0100

    Removed game controller from branch so it can compile and run on heroku

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -12,109 +12,109 @@
  * Copyright 2020 - WebSpace
  */
 
-import { Application } from "express";
-import GameSock, { Lobby, Player } from "@rossmacd/gamesock-server";
+// import { Application } from "express";
+// import GameSock, { Lobby, Player } from "@rossmacd/gamesock-server";
 
-const lobbies: Lobby[] = [];
+// const lobbies: Lobby[] = [];
 
-/**
- * Main Game Controller
- *
- * Handles all functions within a game
- *
- * @param {Application} app
- * @param {boolean} https
- */
-export const gameController = (app: Application, https: boolean) => {
-  /**
-   * Check if the user is authenticated
-   * before allowing them to continue
-   *
-   * Takes in a JWT token
-   *
-   * @param {string} token
-   *
-   * @returns {boolean}
-   */
-  GameSock.onAuth((token: string) => {
-    /**
-     * @todo check JWT token and confirm auth
-     */
+// /**
+//  * Main Game Controller
+//  *
+//  * Handles all functions within a game
+//  *
+//  * @param {Application} app
+//  * @param {boolean} https
+//  */
+// export const gameController = (app: Application, https: boolean) => {
+//   /**
+//    * Check if the user is authenticated
+//    * before allowing them to continue
+//    *
+//    * Takes in a JWT token
+//    *
+//    * @param {string} token
+//    *
+//    * @returns {boolean}
+//    */
+//   GameSock.onAuth((token: string) => {
+//     /**
+//      * @todo check JWT token and confirm auth
+//      */
 
-    return true;
-  });
+//     return true;
+//   });
 
-  /**
-   * On creating a lobby
-   *
-   * @param {ILobby} lobby
-   */
-  GameSock.onLobbyCreate((lobby: Lobby) => {
-    /**
-     * @todo check if the lobby doesn't already exist
-     */
-    if (lobbies.filter((dat) => dat.name === lobby.name).length > 0)
-      return false;
-    console.log("lobby created", lobby);
+//   /**
+//    * On creating a lobby
+//    *
+//    * @param {ILobby} lobby
+//    */
+//   GameSock.onLobbyCreate((lobby: Lobby) => {
+//     /**
+//      * @todo check if the lobby doesn't already exist
+//      */
+//     if (lobbies.filter((dat) => dat.name === lobby.name).length > 0)
+//       return false;
+//     console.log("lobby created", lobby);
 
-    lobbies.push(lobby);
-    return true;
-  });
+//     lobbies.push(lobby);
+//     return true;
+//   });
 
-  /**
-   * On joining a lobby
-   *
-   * @todo return either an empty array or array of players
-   *
-   * @param {string} lobbyName
-   * @param {Player} player
-   */
-  GameSock.onLobbyJoin((lobbyName: string, player: Player) => {
-    /**
-     * @todo check if the lobby allows players
-     */
-    let returnLobbies: Player[] = [];
-    lobbies.forEach((lobby) => {
-      const playerExists = lobby.players.find(
-        (lobbyPlayer: Player) => lobbyPlayer.name === player.name
-      );
-      if (lobby.name === lobbyName && !playerExists) {
-        lobby.players.push(player);
-        returnLobbies = lobby.players;
-      }
-    });
+//   /**
+//    * On joining a lobby
+//    *
+//    * @todo return either an empty array or array of players
+//    *
+//    * @param {string} lobbyName
+//    * @param {Player} player
+//    */
+//   GameSock.onLobbyJoin((lobbyName: string, player: Player) => {
+//     /**
+//      * @todo check if the lobby allows players
+//      */
+//     let returnLobbies: Player[] = [];
+//     lobbies.forEach((lobby) => {
+//       const playerExists = lobby.players.find(
+//         (lobbyPlayer: Player) => lobbyPlayer.name === player.name
+//       );
+//       if (lobby.name === lobbyName && !playerExists) {
+//         lobby.players.push(player);
+//         returnLobbies = lobby.players;
+//       }
+//     });
 
-    return returnLobbies;
-  });
+//     return returnLobbies;
+//   });
 
-  /**
-   * When a player is ready within a lobby
-   *
-   * @param {string} lobbyName
-   * @param {string} playerId
-   */
-  GameSock.onPlayerReady((lobbyName: string, playerId: string) => {
-    /**
-     * @todo if all players are ready start the game
-     */
-    console.log("player ready", lobbyName, playerId);
-    /**
-     * Returning 0 is the host
-     */
-    return 0;
-  });
+//   /**
+//    * When a player is ready within a lobby
+//    *
+//    * @param {string} lobbyName
+//    * @param {string} playerId
+//    */
+//   GameSock.onPlayerReady((lobbyName: string, playerId: string) => {
+//     /**
+//      * @todo if all players are ready start the game
+//      */
+//     console.log("player ready", lobbyName, playerId);
+//     /**
+//      * Returning 0 is the host
+//      */
+//     return 0;
+//   });
 
-  // Update a single player
-  GameSock.onUpdateSinglePlayer((lobbyName: string, player: Player) => {
-    console.log("Updating");
-    // Get the lobby
-    // const lIndex = myLobbies.findIndex(lobby=>lobby.name===lobbyName);
-    player.name = "Ultan";
-    return player;
-  });
+//   // Update a single player
+//   GameSock.onUpdateSinglePlayer((lobbyName: string, player: Player) => {
+//     console.log("Updating");
+//     // Get the lobby
+//     // const lIndex = myLobbies.findIndex(lobby=>lobby.name===lobbyName);
+//     player.name = "Ultan";
+//     return player;
+//   });
 
-  /**
-   * Return the socket server to express
-   */
-  return GameSock.sockServer(app, https);
-};
+//   /**
+//    * Return the socket server to express
+//    */
+//   return GameSock.sockServer(app, https);
+// };
