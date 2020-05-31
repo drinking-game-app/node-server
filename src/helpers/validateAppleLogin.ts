5177698066218c38d10bf194315cda11812dc7fb
Eoan O Dea <odeaeoan@gmail.com>
2020-05-31 17:03:21 +0100

    More apple token testing

--- src/helpers/validateAppleLogin.ts
+++ src/helpers/validateAppleLogin.ts
@@ -71,7 +71,9 @@ export const loginWithApple = (type: string, token: string) => {
         },
       });
       // if(res.body.error)
-      console.log('apple response!', res.data, 'user ID!!!', getUserId(res.data.id_token))
+      console.log('apple response!', res.data)
+
+      console.log('user ID!!!', getUserId(res.data.id_token))
 
       /**
        * Create the user from the res payload
