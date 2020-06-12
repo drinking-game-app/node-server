de7b102a56852d739de80392a393e1cdd77a4b34
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2020-06-12 22:39:29 +0100

    Fixed mege errors

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,6 @@ import app from './express'
 import mongoose from 'mongoose'
 import Session from 'express-session'
 
-// Redis Database
-import { createClient as createRedisClient } from 'redis';
-// import connectRedis from 'connect-redis';
-
-// @ts-ignore
-// import sockServer from '@rossmacd/gamesock-server'
-import {sockServer,onAuth,onLobbyCreate,onLobbyJoin,Lobby,Player,onPlayerReady} from '@rossmacd/gamesock-server'
 
 /**
  * Mongoose Connection configurations
@@ -55,78 +48,10 @@ mongoose.connection.on('error', () => {
 })
 
 
-// // Configuration about your Redis session data structure.
-// const redisClient = createRedisClient();
-// // const RedisStore = connectRedis(Session);
-
-// redisClient.on("error", (error) =>{
-//   console.error(error);
-// });
-
-// redisClient.set("key", "lovelyTest");
-// redisClient.get("key", (err,key)=>{
-//   if(err){
-//     console.log(err);
-//   }else{
-//   console.log(key);
-// }
-// });
-
-// Game Code
-// override auth function
-// gamesock.onAuth((token:string)=>{
-//   console.log(token);
-//   return true
-// });
-const myLobbies:Lobby[]=[];
-
-app.get('/stats',(req,res)=>{
-  let html=`<h1>Lobbies</h1>`
-  myLobbies.forEach((lobby,index)=>html=html+`<h5>Lobby ${index}</h5><p>${ JSON.stringify(lobby)}</p>`)
-  return res.status(200).send(html)}
-)
-
-// Authorize function
-onAuth((token: string) => {
-  return true;
-});
-
-// Push lobby into local array
-onLobbyCreate((newLobby)=>{
-  myLobbies.push(newLobby);
-  return true
-})
-
-// Push player into their lobby
-onLobbyJoin((lobbyName, player)=>{
-  const plIndex = myLobbies.findIndex(lobby=>lobby.name===lobbyName);
-  // don't join lobby if it dosnt exist
-  if(plIndex===-1) return false
-  // Add player to lobby
-  myLobbies[plIndex].players.push(player);
-  return true
-})
-
-// Set player Status to ready
-onPlayerReady((lobbyName:string, playerId:string)=>{
-  // Get the lobby
-  const lIndex = myLobbies.findIndex(lobby=>lobby.name===lobbyName);
-  // Get the player
-  const pIndex = myLobbies[lIndex].players.findIndex((player:Player)=>player.id===playerId);
-  // Set the status to ready server-side
-  myLobbies[lIndex].players[pIndex].ready=true;
-  // Broadcast the playerNumber back to the others
-  return pIndex
-})
-
-// Create http/s server
-const server = sockServer(app, false);
-
-
 /**
  * Listen on the specified port, and for any errors
  */
-server.listen(config.port, () => {
+app.listen(config.port, () => {
   console.info('Server started on port %s.', config.port)
 })
 .on("error", (err: any) => {
